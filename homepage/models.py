3fbc5c82c23924bd682f6714ed9670ce5ab95dcc
Doug <secretsighter@gmail.com>
2015-01-12 08:02:52 -0700

    updating models

--- homepage/models.py
+++ homepage/models.py
@@ -8,17 +8,20 @@ class Student(models.Model):
     student_number = models.AutoField()
     average_mark = models.CharField(max_length=1)
 
-class Enrollment(models.Model):
-    marks_received = models.CharField()
-
-class Seminary(models.Model):
+class Seminar(models.Model):
     name = models.CharField(max_length=50)
     seminar_name = models.CharField(max_length=50)
-    fees = models.
+    fees = models.DecimalField(max_digits=6, decimal_places=2)
+    professor = models.ForeignKey('Professor')
+
+class Enrollment(models.Model):
+    marks_received = models.CharField()
+    student = models.ForeignKey('Student')
+    seminar = models.ForeignKey('Seminar')
 
 class Professor(models.Model):
-    name
-    address
-    phone_number
-    email_address
-    salary
+    name = models.CharField(max_length=50)
+    address = models.CharField(max_length=100)
+    phone_number = models.CharField(max_length=15)
+    email_address = models.CharField(max_length=100)
+    salary = models.DecimalField(max_digits=9, decimal_places=2)
